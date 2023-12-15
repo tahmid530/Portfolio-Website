9c6a64b1ef7699c7575f0bd7a2fa55f817bb973d
tahmid530 <tahmidahmed530@gmail.com>
2023-12-15 21:00:27 +0600

    skills section completed

--- src/Pages/Resume/Skills/Skills.jsx
+++ src/Pages/Resume/Skills/Skills.jsx
@@ -3,7 +3,22 @@
 const Skills = () => {
     return (
         <>
-            <h3 className="text-3xl text-center font-bold pb-10">Skills</h3>
+            <div className="bg-base-200 p-96">
+                <h3 className="text-3xl text-center font-bold pb-10">Skills</h3>
+                <div className="flex justify-center gap-6">
+                    <img src="https://i.ibb.co/jyMnVcw/html-5.png" alt="" className="h-24 w-24 hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/yWg5Vvv/css-3.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/tJcdMrp/tailwind.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/m6TK3vv/bootstrap.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/Byyb1sy/github.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/K6PT4cz/js.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/Z29njGk/react.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/Q8YZsdT/firebase.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/w4kQ9dN/nodejs.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/0XdjrBB/express-js.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                    <img src="https://i.ibb.co/94S7ZTT/mongodb.png" alt="" className="h-24 w-24  hover:shadow-lg hover:shadow-cyan-500/100" />
+                </div>
+            </div>
         </>
     );
 };
