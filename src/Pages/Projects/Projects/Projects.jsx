44b4d26b3667f0dd3018fe6df7071ba37e7d255f
tahmid530 <tahmidahmed530@gmail.com>
2023-12-16 01:50:00 +0600

    projects section completed

--- src/Pages/Projects/Projects/Projects.jsx
+++ src/Pages/Projects/Projects/Projects.jsx
@@ -3,7 +3,59 @@
 const Projects = () => {
     return (
         <>
-           
+            <div>
+                <div className="flex items-center justify-evenly mt-10 mb-28">
+                    <div className="bg-base-200 p-8" data-aos="zoom-out-right">
+                        <img src="https://i.ibb.co/HpyfKs4/COURSE-REGISTRATION.png" alt="" className="w-[640px]" />
+                    </div>
+                    <div className="lg:ml-40" data-aos="zoom-out-left">
+                        <h3 className="text-2xl font-bold">Projects-1: Course Registration</h3>
+                        <p className="my-8">
+                            Explore the world of web development with the Course Registration Webpage a simple yet <br />
+                            dynamic JavaScript project. This web page offers practical functionalities such as course credit <br />
+                            calculations, dynamic course pricing, and an intuitive cart option.
+                        </p>
+                        <div className="flex gap-3">
+                            <a href="https://github.com/tahmid530/Course-Registration-Webpage?tab=readme-ov-file"><button className="btn btn-outline hover:bg-[#FF3B25] bg-white hover:text-black border-2">Code Link</button></a>
+                            <a href="https://probable-trick.surge.sh"><button className="btn btn-outline hover:bg-[#FF3B25] bg-white hover:text-black border-2">Live Site</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-evenly mt-10 mb-28" data-aos="zoom-out">
+                    <div className="bg-base-200 p-8" data-aos="zoom-out-right">
+                        <img src="https://i.ibb.co/mzLMNRy/brand-shop.png" alt="" className="w-[640px]" />
+                    </div>
+                    <div className="lg:ml-40" data-aos="zoom-out-left">
+                        <h3 className="text-2xl font-bold">Projects-2: TechFusion</h3>
+                        <p className="my-8">
+                            Tech Fusion: Elevate your lifestyle with cutting-edge tech and renowned brands. Shop hassle-free <br />
+                            for high-quality electronics and accessories. Welcome to the fusion of style and innovation!
+                        </p>
+                        <div className="flex gap-3">
+                            <a href="https://github.com/tahmid530/Brand-Shop-Client?tab=readme-ov-file"><button className="btn btn-outline hover:bg-[#FF3B25] bg-white hover:text-black border-2">Code Link</button></a>
+                            <a href="https://assignment-10-e2030.web.app"><button className="btn btn-outline hover:bg-[#FF3B25] bg-white hover:text-black border-2">Live Site</button></a>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex items-center justify-evenly mt-10 mb-28" data-aos="zoom-out">
+                    <div className="bg-base-200 p-8" data-aos="zoom-out-right">
+                        <img src="https://i.ibb.co/sKc6yZs/career-connector.png" alt="" className="w-[640px]" />
+                    </div>
+                    <div className="lg:ml-40" data-aos="zoom-out-left">
+                        <h3 className="text-2xl font-bold">Projects-3: Career Connector</h3>
+                        <p className="my-8">
+                            Introducing Career Connector, a beginner-friendly web development project created for learning. <br />
+                            This simple website explores the basics of online platform creation, offering a glimpse <br />
+                            into essential web development concepts and skills. Ideal for beginners, Career Connector lays the <br />
+                            foundation for crafting functional and visually appealing webpages.
+                        </p>
+                        <div className="flex gap-3">
+                            <a href="https://github.com/tahmid530/Career-Connector-Client?tab=readme-ov-file"><button className="btn btn-outline hover:bg-[#FF3B25] bg-white hover:text-black border-2">Code Link</button></a>
+                            <a href="https://assignment-11-cd7b0.web.app"><button className="btn btn-outline hover:bg-[#FF3B25] bg-white hover:text-black border-2">Live Site</button></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     );
 };
